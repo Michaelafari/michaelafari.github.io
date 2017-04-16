95d3ff38b9e713b663dd4cd06412cb2a5171a6e4
Michaelafari <michaelafariytube@gmail.com>
2017-04-16 06:18:30 -0700

    Update and rename README.md to index.php

--- README.md
+++
@@ -1,1 +0,0 @@
-# michaelafari.github.io
---
+++ index.php
@@ -0,0 +1,9 @@
+<DOCTYPE html!>
+<html>
+<head>
+<title>michaelafari</title>
+</head>
+<body>
+<h2>Michael Afari Welcome to you Home page</h2>
+</body>
+</html>
